5e42b2358a7f37e9ff2e6378708c51ea075214fd
Luiz Davi <55983920+luiz-davi@users.noreply.github.com>
2021-08-27 18:55:30 -0300

    Adicionando Componentes a página inicial

--- lib/screens/task_screen_page.dart
+++ lib/screens/task_screen_page.dart
@@ -1,3 +1,6 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class TasksScreen extends StatelessWidget {
@@ -6,22 +9,61 @@ class TasksScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        padding: const EdgeInsets.only(top: 60, left: 30),
-        child: Column(
-          children: const [
-            CircleAvatar(
-              backgroundColor: Colors.white,
-              radius: 35,
-              child: Icon(
-                Icons.list,
-                size: 35,
-                color: Colors.lightBlueAccent,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: const EdgeInsets.only(top: 60, left: 30, bottom: 70),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                CircleAvatar(
+                  backgroundColor: Colors.white,
+                  radius: 35,
+                  child: Icon(
+                    Icons.list,
+                    size: 35,
+                    color: Colors.lightBlueAccent,
+                  ),
+                ),
+                SizedBox(height: 25),
+                Text(
+                  "Todoey",
+                  style: TextStyle(
+                    fontSize: 50,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.white,
+                  ),
+                ),
+                Text(
+                  "12 Tarefas",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Flexible(
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(35),
+                  topRight: Radius.circular(35),
+                ),
               ),
             ),
-            SizedBox(height: 10),
-            Text("Todoey"),
-          ],
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.lightBlueAccent,
+        onPressed: () {},
+        child: Icon(
+          Icons.add,
         ),
       ),
     );
